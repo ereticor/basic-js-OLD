94abf33e9e34dde4be71a94176c0c3ad9ac00910
ereticor <ereticor@gmail.com>
2021-03-26 12:22:08 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,82 @@
-const CustomError = require("../extensions/custom-error");
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (flexMachine) {
+    this.machine = (flexMachine === false);
+  }
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encrypted = "";
+    let convertedKey = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i]) == -1) {
+        convertedKey += message[i];
+      }
+      else {
+        if (j >= key.length) {
+          j = 0;
+        }
+        convertedKey += key[j];
+        j++;
+      }
+    }
+    for (let i = 0; i < message.length; i++) {
+      let shift = alphabet.indexOf(message[i]) + alphabet.indexOf(convertedKey[i]);
+      if (alphabet.indexOf(message[i]) == -1) {
+        encrypted += message[i];
+      }
+      else if (shift <= 25) {
+        encrypted += alphabet[shift];
+      }
+      else {
+        encrypted += alphabet[shift - 26];
+      }
+    }
+    if (this.machine) {
+      return encrypted.split("").reverse().join("");
+    }
+    return encrypted;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error();
+    }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let decrypted = "";
+    let convertedKey = "";
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if (alphabet.indexOf(encryptedMessage[i]) == -1) {
+        convertedKey += encryptedMessage[i];
+      }
+      else {
+        if (j >= key.length) {
+          j = 0;
+        }
+        convertedKey += key[j];
+        j++;
+      }
+    }
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let shift = alphabet.indexOf(encryptedMessage[i]) - alphabet.indexOf(convertedKey[i]);
+      if (alphabet.indexOf(encryptedMessage[i]) == -1) {
+        decrypted += encryptedMessage[i];
+      }
+      else if (shift < 0) {
+        decrypted += alphabet[shift + 26];
+      }
+      else {
+        decrypted += alphabet[shift];
+      }
+    }
+    if (this.machine) {
+      return decrypted.split("").reverse().join("");
+    }
+    return decrypted;
   }
 }
 
